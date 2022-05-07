7d5d8123f95f46496ac74a2d0320411d97480786
Lash Blast <evelyn.aguirre.9895@gmail.com>
2022-05-06 22:38:25 -0500

    initial file setup

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+// const { MODELS GO HERE } = require("../models");
+const withAuth = require("../utils/auth");
+
+// code goes here
+
+module.exports = router;
---
+++ controllers/home-routes.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+// const { MODELS GO HERE } = require("../models");
+
+// code goes here
+
+module.exports = router;
