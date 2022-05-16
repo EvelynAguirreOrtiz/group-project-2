167bea8567c4a360d83807dd28249b3513cec7dc
Amanda Dell <amhernandez1163@gmail.com>
2022-05-16 18:56:24 -0500

    additional styling and clean up to UI

---
+++ public/javascript/script.js
@@ -0,0 +1,21 @@
+var granimInstance = new Granim({
+  element: "#canvas-complex",
+  direction: "left-right",
+  isPausedWhenNotInView: true,
+  states: {
+    "default-state": {
+      gradients: [
+        [
+          { color: "#833ab4", pos: 0.2 },
+          { color: "#fd1d1d", pos: 0.8 },
+          { color: "#38ef7d", pos: 1 },
+        ],
+        [
+          { color: "#40e0d0", pos: 0 },
+          { color: "#ff8c00", pos: 0.2 },
+          { color: "#ff0080", pos: 0.75 },
+        ],
+      ],
+    },
+  },
+});
